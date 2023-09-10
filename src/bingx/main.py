b078ae42b65d4a03423547ab1151651eaa2155be
web3sdruvs.crypto <112975076+web3sdruvs@users.noreply.github.com>
2023-09-10 15:57:15 -0300

    adjsut main

--- src/bingx/main.py
+++ src/bingx/main.py
@@ -5,4 +5,4 @@ import order
 balance.get_balance("BTC") #(symbol)
 withdraw.request_withdraw("BTC", "Bitcoin", "0x00001", "coldwallet", 1) #(symbol, network, address, addressTag, qty)
 order.create_order("BTC", 0.1) #(symbol, quantity)
-order.create_order("BTC", 123456789) #(symbol, orderId)
+order.cancel_order("BTC", 123456789) #(symbol, orderId)
