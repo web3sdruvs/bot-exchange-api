5d9111747f18a407bbf44c4ead4709f6dd476a7c
web3sdruvs.crypto <112975076+web3sdruvs@users.noreply.github.com>
2024-02-26 08:58:22 -0300

    docstring credential

--- src/bingx/credential.py
+++ src/bingx/credential.py
@@ -8,13 +8,33 @@ APIURL_BINGX = 'https://open-api.bingx.com'
 APIKEY_BINGX = 'YOUR API KEY'
 SECRETKEY_BINGX = 'YOUR SECRET KEY'
 
-#get sign request secretkey to generate a signature
 def get_sign(api_secret, payload):
+    """
+    Generate a signature for authenticating an API request.
+
+    Parameters:
+    - api_secret (str): API secret key.
+    - payload (str): Request data to be signed.
+
+    Returns:
+    - str: Generated signature as a hexadecimal string.
+    """
     signature = hmac.new(api_secret.encode('utf-8'), payload.encode('utf-8'), digestmod=sha256).hexdigest()
     return signature
 
-#single request api call
 def send_request(method, path, urlpa, payload):
+    """
+    Make a call to the BingX API.
+
+    Parameters:
+    - method (str): HTTP method of the request (e.g., 'GET', 'POST').
+    - path (str): API endpoint path.
+    - urlpa (str): URL parameters.
+    - payload (str): Request data.
+
+    Returns:
+    - str: Content of the API response.
+    """
     url = '%s%s?%s&signature=%s' % (APIURL_BINGX, path, urlpa, get_sign(SECRETKEY_BINGX, urlpa))
     headers = {
         'X-BX-APIKEY': APIKEY_BINGX,
@@ -22,8 +42,16 @@ def send_request(method, path, urlpa, payload):
     response = requests.request(method, url, headers=headers, data=payload)
     return response.text
 
-#parameter encapsulation
 def praseParam(params_map):
+    """
+    Encapsulate parameters in a specific format for an API request.
+
+    Parameters:
+    - params_map (dict): Dictionary containing the request parameters.
+
+    Returns:
+    - str: Formatted string of parameters with timestamp added.
+    """
     sortedKeys = sorted(params_map)
     params_str = '&'.join(['%s=%s' % (x, params_map[x]) for x in sortedKeys])
     return params_str+'&timestamp='+str(int(time.time() * 1000))
