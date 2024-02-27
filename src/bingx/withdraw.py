9b184014dcd0d25cf222377dd4373954e37428bf
web3sdruvs.crypto <112975076+web3sdruvs@users.noreply.github.com>
2024-02-27 08:56:00 -0300

    docstring withdraw

--- src/bingx/withdraw.py
+++ src/bingx/withdraw.py
@@ -10,6 +10,21 @@ the sending address along with the address tag on the exchange (you can find thi
 step on the exchange, next to the Address field, next the list icon)
 '''
 def withdraw(symbol, network, address, addressTag, qty, walletType):
+    """
+    Initiate a withdrawal request for a specific cryptocurrency.
+
+    Parameters:
+    - symbol (str): The cryptocurrency symbol (e.g., 'BTC', 'ETH').
+    - network (str): The network for the withdrawal (e.g., 'BTC', 'ERC20').
+    - address (str): The destination address for the withdrawal.
+    - addressTag (str): Address tag or memo for certain cryptocurrencies.
+    - qty (float): The amount of cryptocurrency to withdraw.
+    - walletType (str): 1 = fund account | 2 = standard account | 3 = perpetual account.
+
+    Returns:
+    - int: If successful, returns the withdrawal request ID.
+    - str: If an error occurs, returns the error message.
+    """
     payload = {}
     path = '/openApi/wallets/v1/capital/withdraw/apply'
     method = 'POST'
@@ -19,7 +34,7 @@ def withdraw(symbol, network, address, addressTag, qty, walletType):
     'address': address,
     'addressTag': addressTag,
     'amount': qty,
-    'walletType': walletType # 1 = fund account | 2 = standard account | 3 = perpetual account
+    'walletType': walletType 
     }
     params_str = credential.praseParam(params_map)
     json_data = json.loads(credential.send_request(method, path, params_str, payload))
@@ -33,6 +48,17 @@ def withdraw(symbol, network, address, addressTag, qty, walletType):
         return  error
 
 def withdrawfee(symbol, network):
+    """
+    Retrieve the withdrawal fee for a specific cryptocurrency and network.
+
+    Parameters:
+    - symbol (str): The cryptocurrency symbol (e.g., 'BTC', 'ETH').
+    - network (str): The network for the withdrawal (e.g., 'BTC', 'ERC20').
+
+    Returns:
+    - float: The withdrawal fee for the specified cryptocurrency and network.
+    - str: If an error occurs, returns the error message.
+    """
     payload = {}
     path = '/openApi/wallets/v1/capital/config/getall'
     method = "GET"
